f8131fbd429ee1f9573367da6d856ed4b1bde44d
sparkh <sparkh42@gmail.com>
2015-12-07 23:41:54 +0800

    Xcode init
    A commit for the initial Xcode file

---
+++ NestedWorld/NestedWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NestedWorld
+//
+//  Created by Jean-Antoine Dupont on 07/12/2015.
+//  Copyright © 2015 NestedWorld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
