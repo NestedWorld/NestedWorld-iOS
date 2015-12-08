5732c2bb89ea5daa711c2ff960defabfe626286b
sparkh <sparkh42@gmail.com>
2015-12-08 19:54:28 +0800

    Create login view files
    Commit that will prepare the login view implementation

---
+++ NestedWorld/NestedWorld/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  NestedWorld
+//
+//  Created by Jean-Antoine Dupont on 08/12/2015.
+//  Copyright © 2015 NestedWorld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
