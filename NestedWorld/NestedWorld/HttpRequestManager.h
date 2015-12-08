f706b3a624110df017aafe9c0733f8ce4c7c696d
sparkh <sparkh42@gmail.com>
2015-12-08 12:16:46 +0800

    Add HttpRequestManager files
    Just empty files for preparing the implementation of the
    HttpRequestManager that will work with AFNetworking

---
+++ NestedWorld/NestedWorld/HttpRequestManager.h
@@ -0,0 +1,13 @@
+//
+//  HttpRequestManager.h
+//  NestedWorld
+//
+//  Created by Jean-Antoine Dupont on 07/12/2015.
+//  Copyright © 2015 NestedWorld. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HttpRequestManager : NSObject
+
+@end
---
+++ NestedWorld/NestedWorld/HttpRequestManager.m
@@ -0,0 +1,13 @@
+//
+//  HttpRequestManager.m
+//  NestedWorld
+//
+//  Created by Jean-Antoine Dupont on 07/12/2015.
+//  Copyright © 2015 NestedWorld. All rights reserved.
+//
+
+#import "HttpRequestManager.h"
+
+@implementation HttpRequestManager
+
+@end
