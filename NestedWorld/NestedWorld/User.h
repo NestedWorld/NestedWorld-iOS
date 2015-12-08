8cbe55f0408ab3639e1d5a6265db434c120f3f34
sparkh <sparkh42@gmail.com>
2015-12-08 18:59:24 +0800

    Create User file
    Just creating the user files for preparing the user implementation

---
+++ NestedWorld/NestedWorld/User.h
@@ -0,0 +1,13 @@
+//
+//  User.h
+//  NestedWorld
+//
+//  Created by Jean-Antoine Dupont on 08/12/2015.
+//  Copyright © 2015 NestedWorld. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@end
---
+++ NestedWorld/NestedWorld/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  NestedWorld
+//
+//  Created by Jean-Antoine Dupont on 08/12/2015.
+//  Copyright © 2015 NestedWorld. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
