fe7f8f0500c790c74becb8c42b4f6815123b528f
sparkh <sparkh42@gmail.com>
2015-12-08 19:53:58 +0800

    Create register view files
    Commit that will prepare the register view implementation

---
+++ NestedWorld/NestedWorld/RegisterViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegisterViewController.h
+//  NestedWorld
+//
+//  Created by Jean-Antoine Dupont on 08/12/2015.
+//  Copyright © 2015 NestedWorld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegisterViewController : UIViewController
+
+@end
