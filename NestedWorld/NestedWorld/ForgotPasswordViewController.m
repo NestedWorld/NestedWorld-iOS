c9c2bdee4337f4591143204ab25b4e642aa81bdb
sparkh <sparkh42@gmail.com>
2015-12-09 22:46:21 +0800

    Add forgot password view files
    Add the forgot password files and prepare the implementation

---
+++ NestedWorld/NestedWorld/ForgotPasswordViewController.m
@@ -0,0 +1,39 @@
+//
+//  ForgotPasswordViewController.m
+//  NestedWorld
+//
+//  Created by Jean-Antoine Dupont on 09/12/2015.
+//  Copyright © 2015 NestedWorld. All rights reserved.
+//
+
+#import "ForgotPasswordViewController.h"
+
+@interface ForgotPasswordViewController ()
+
+@end
+
+@implementation ForgotPasswordViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
