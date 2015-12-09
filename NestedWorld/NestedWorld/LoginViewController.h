5635a0d2d90c77b829417c983c5dbd0a0e085810
sparkh <sparkh42@gmail.com>
2015-12-09 15:11:42 +0800

    LoginView design
    Create the design of the login view
    Set the constraints
    Add the component of the login view as properties in the
    LoginViewController.h file

--- NestedWorld/NestedWorld/LoginViewController.h
+++ NestedWorld/NestedWorld/LoginViewController.h
@@ -7,7 +7,26 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "APIRequestManager.h"
+#import "User.h"
 
-@interface LoginViewController : UIViewController
+@interface LoginViewController : UIViewController {
+    APIRequestManager *apiManager;
+    User *user;
+}
+
+// Label
+@property (weak, nonatomic) IBOutlet UILabel *infoLabel;
+
+// TextField
+@property (weak, nonatomic) IBOutlet UITextField *emailField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+
+// Button
+@property (weak, nonatomic) IBOutlet UIButton *connectionButton;
+@property (weak, nonatomic) IBOutlet UIButton *registerButton;
+@property (weak, nonatomic) IBOutlet UIButton *forgotPasswordButton;
+@property (weak, nonatomic) IBOutlet UIButton *settingsButton;
+
 
 @end
